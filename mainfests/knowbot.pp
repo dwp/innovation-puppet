65361362f58c8f41b98f5e03b751aa9078e820fb
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-12 14:12:46 +0100

    Initial setup of the puppet repository with freepuppet bootstrapping.

---
+++ mainfests/knowbot.pp
@@ -0,0 +1,18 @@
+
+/**
+ * Node used for running the knowbot related application infrastructure.
+ */
+node 'knowbot-app' {
+  
+	include freepuppet
+  
+}
+
+/**
+ * Node used for runnin the Spark / EMR enviroment for generating the social graph.
+ */
+node 'knowbot-spark' {
+	
+	include freepuppet
+	
+}
---
+++ modues/freepuppet/manifests/init.pp
@@ -0,0 +1,22 @@
+
+#
+# freepuppet moduled used to install the basics of a masterless puppet configuration.
+# @author jonty.bale@dwp.gsi.gov.uk
+#
+class freepuppet {
+	
+	# ensure the run script is in place
+	file { "/usr/local/bin/freepuppet-run":
+		ensure => present,
+		source => "puppet:///modules/freepuppet/files/cron/run.sh"
+	}
+	
+	# ensure we have a cron in place which runs every 15min
+	cron { "freepuppet-run":
+		ensure => present,
+		command => "/usr/local/bin/freepuppet-run"
+		minute  => '*/15',
+	}
+	
+	
+}
