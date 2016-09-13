23e672d7c1fbdba9eecbb6c245c7266d46808131
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-13 14:55:50 +0100

    Minor lint issues addressed.

--- manifests/knowbot.pp
+++ manifests/knowbot.pp
@@ -1,21 +1,17 @@
 
-/**
-* Node used for running the knowbot related application infrastructure.
-*/
+# Node used for running the knowbot related application infrastructure.
 node 'knowbot-app'
 {
-    include bootstrap
+    include ::bootstrap
     # setup docker environment
-    include docker
-    class { 'docker::compose' : 
+    include ::docker
+    class { '::docker::compose' :
         ensure => present
     }
 }
 
-/**
-* Node used for runnin the Spark / EMR enviroment for generating the social graph.
-*/
+# Node used for runnin the Spark / EMR enviroment for generating the social graph.
 node 'knowbot-spark'
 {
-    include bootstrap
+    include ::bootstrap
 }
